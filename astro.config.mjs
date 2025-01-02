02359652437e6b33f26264d4b85516a585cbeb11
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-01-02 01:50:54 -0600

    rah

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,20 @@
 import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://astro.build/config
-// export default defineConfig({
-//    site: 'https://team.pluton',
-//    server: { 
-//        host: true,
-//        port: 8080
-//    },
-// });
+https://astro.build/config
 export default defineConfig({
-    vite: {
-      plugins: [basicSsl()],
-      server: {
+    site: 'https://team.pluton',
+    server: { 
         host: true,
         port: 8080
-      },
     },
-  });
+});
+//export default defineConfig({
+//    vite: {
+//      plugins: [basicSsl()],
+//      server: {
+//        host: true,
+//        port: 8080
+//      },
+//    },
+//  });
