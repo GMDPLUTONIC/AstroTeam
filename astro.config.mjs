6576ffa9f41ba577a23df82843000b0acfd07de4
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-01-02 01:59:03 -0600

    brhu

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-- https://astro.build/config
+// https://astro.build/config
 export default defineConfig({
     site: 'https://team.pluton',
     server: { 
