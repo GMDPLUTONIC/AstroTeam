29dcad75217c882270e52ec3c63072aa8985088a
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-01-03 20:00:49 -0600

    upload to firebase and finish the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://team.pluton',
-    server: { 
-        host: true,
-        port: 8080
+    vite: {
+      plugins: [basicSsl()],
+      server: {
+        https: true,
+      },
     },
-});
+  })
